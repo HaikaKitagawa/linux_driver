9b0dc5c77ea2fba55a457f4c54692312cdd2332d
HaikaKitagawa <1031168824@qq.com>
2026-05-23 19:04:06 +0800

    folder created

---
+++ 01_chrdevbase/chrdevbase.c
@@ -0,0 +1,17 @@
+/*
+ * @Author: Haika 1031168824@qq.com
+ * @Date: 2026-05-23 18:18:17
+ * @LastEditors: Haika 1031168824@qq.com
+ * @LastEditTime: 2026-05-23 18:58:45
+ * @FilePath: /linux_driver/01_chrdevbase/chrdevbase.c
+ * @Description: 
+ * 
+ * Copyright (c) 2026 by Haika, All Rights Reserved. 
+ */
+
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/delay.h>
+#include <linux/ide.h>
+#include <linux/init.h>
+#include <linux/module.h>

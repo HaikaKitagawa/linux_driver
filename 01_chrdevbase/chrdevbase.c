338b5aeab23e47c6fb8e6c8abeaeeb39b3d34c99
HaikaKitagawa <1031168824@qq.com>
2026-05-23 19:08:47 +0800

    update LastEditTime and add device definitions in chrdevbase.c

--- 01_chrdevbase/chrdevbase.c
+++ 01_chrdevbase/chrdevbase.c
@@ -2,7 +2,7 @@
  * @Author: Haika 1031168824@qq.com
  * @Date: 2026-05-23 18:18:17
  * @LastEditors: HaikaKitagawa 1031168824@qq.com
- * @LastEditTime: 2026-05-23 19:05:33
+ * @LastEditTime: 2026-05-23 19:08:24
  * @FilePath: /linux_driver/01_chrdevbase/chrdevbase.c
  * @Description: A char device example.
  * 
@@ -15,3 +15,7 @@
 #include <linux/ide.h>
 #include <linux/init.h>
 #include <linux/module.h>
+
+#define CHARDEVBASE_MAJOR 200 /* 预设的主设备号 */
+#define CHARDEVBASE_NAME "chardevbase" /* 设备名称 */
+
